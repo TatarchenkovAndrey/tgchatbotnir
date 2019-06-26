7bfdfe2e51e3d3f187240651cc0b7ba8c142cbc9
Andrey <tatarchenkov96@gmail.com>
2019-06-27 03:39:03 +0500

    s

--- main.py
+++ main.py
@@ -6,21 +6,23 @@ from deeppavlov.core.common.file import read_json
 from deeppavlov.core.commands.infer import build_model
 from deeppavlov.core.commands.train import train_evaluate_model_from_config
 
-
+print("import successful")
 far = train_evaluate_model_from_config(configs.faq.tfidf_logreg_en_faq)
 faq = build_model(configs.faq.tfidf_logreg_en_faq, download = True)
 model_config = read_json("./config.json")
 model_config["dataset_reader"]["data_path"] = "./faq_school_en.csv"
 model_config["dataset_reader"]["data_url"] = None
 faq = train_model(model_config)
-
+print("train model")
 bot = telebot.TeleBot('301914397:AAEmR8WlfzyxQT53zdpqHrSwR8iwaKEr-h8')
 
 def GetAnswer(question):
+    print("get question")
     return faq([question])[0][0][0]
 
 @bot.message_handler(content_types=['text'])
-def get_text_messages(message):    
+def get_text_messages(message):
+    print("text handler")
     if message.text == "Привет":
         bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
     elif message.text == "/help":
